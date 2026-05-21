8e19fa7546865a194fb2703d4fa4806f3f240246
Asad Chowdhury <asadchowdhury@Asads-Mac-Studio.local>
2026-05-21 22:36:46 +0600

    deployment

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,6 +1,18 @@
 import { jwtClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
+
 export const authClient = createAuthClient({
-  baseURL: process.env.BETTER_AUTH_URL,
   plugins: [jwtClient()],
 });
+
+export const getAuthHeaders = async () => {
+  const { data, error } = await authClient.token();
+
+  if (error || !data?.token) {
+    return {};
+  }
+
+  return {
+    authorization: `Bearer ${data.token}`,
+  };
+};
